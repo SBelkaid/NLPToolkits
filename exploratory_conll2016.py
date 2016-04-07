8facfd3ee10a9a58c9b1d9dda0a95c86b695911e
Soufyan Belkaid <soufyanbelkaid@MacBook-Pro-2.local>
2016-04-07 12:19:25 +0200

    update

--- exploratory_conll2016.py
+++ exploratory_conll2016.py
@@ -5,12 +5,16 @@ import os
 from sklearn.feature_extraction import DictVectorizer
 
 
-ENGLISH_ = 'tutorial/conll16st-en-01-12-16-trial'
+ENGLISH_TRIAL = 'tutorial/conll16st-en-01-12-16-trial'
+ENGLISH_TRAIN = 'conll16st-en-zh-dev-train_LDC2016E50/conll16st-en-01-12-16-train'
+
+#SET THE USE VARIABLE TO EITHER ENGLISH TRIAL OR THE ENGLISH TRAIN
+USE = ENGLISH_TRAIN
 
 
-pdtb_file = codecs.open(ENGLISH_+'/relations.json', encoding='utf8')
+pdtb_file = codecs.open(USE+'/relations.json', encoding='utf8')
 relations = [json.loads(x) for x in pdtb_file]
-parse_file = codecs.open('tutorial/conll16st-en-01-12-16-trial/parses.json', encoding='utf8')
+parse_file = codecs.open(USE+'/parses.json', encoding='utf8')
 en_parse_dict = json.load(parse_file)
 en_example_relation = relations[10]
 en_doc_id = en_example_relation['DocID']
@@ -18,8 +22,16 @@ en_doc_id = en_example_relation['DocID']
 
 some_features = []
 for rel in relations:
+	rel_id = rel['ID']
+	connective = rel['Connective']['RawText']
+	type_relation = rel['Type'] 
+	sense_relation = rel['Sense'][0]
+	arg1_raw = rel['Arg1']['RawText']
+	arg1_token_list = rel['Arg1']['TokenList']
+	# print connective, '\t\t',type_relation,'\t\t',sense_relation, '\t\t', arg1_raw
 	# some_features.append({rel['ID']:rel['Connective'], 'tokens_connective':rel['Connective']['TokenList']})
-	print rel['Connective']['RawText']
+	some_features.append({'id':rel_id, 'connective': connective, 'type': type_relation, 'arg1_raw':arg1_raw})
+	# 'arg1_token_list':arg1_token_list
 
 
 # print "THIS IS A PARSETREE" 
@@ -35,4 +47,5 @@ for rel in relations:
 
 
 vec = DictVectorizer()
-# X = vec.fit_transform(some_features)
+X = vec.fit_transform(some_features)
+print vec.get_feature_names()
