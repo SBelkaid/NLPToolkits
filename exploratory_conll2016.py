1e6c54bd09bc4ebb50f401f2bfe4ef7d4f816fd7
Soufyan Belkaid <soufyanbelkaid@MacBook-Pro-2.local>
2016-04-07 10:11:48 +0200

    example

--- exploratory_conll2016.py
+++ exploratory_conll2016.py
@@ -1,6 +1,10 @@
 import json
 import codecs
-pdtb_file = codecs.open('conll16st-en-01-12-16-trial/relations.json', encoding='utf8')
-relations = [json.loads(x) for x in pdtb_file];
+import os
+
+
+ENGLISH_ = 'tutorial/conll16st-en-01-12-16-trial'
+pdtb_file = codecs.open(ENGLISH_+'/relations.json', encoding='utf8')
+relations = [json.loads(x) for x in pdtb_file]
 example_relation = relations[10]
-example_relation
+print example_relation
