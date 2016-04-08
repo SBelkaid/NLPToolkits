f543a4c65a61f63ab12fe2f311e3c518b956b850
Soufyan Belkaid <soufyanbelkaid@MacBook-Pro-2.local>
2016-04-08 17:47:57 +0200

    extracted some data from the fiven dictionairy

--- exploratory_conll2016.py
+++ exploratory_conll2016.py
@@ -18,43 +18,45 @@ en_parse_dict = json.load(codecs.open(USE+'/parses.json', encoding='utf8'))
 # en_example_relation = relations[10]
 # en_doc_id = en_example_relation['DocID']
 
+def extract_data():
+	all_features = []
+	some_features = []
+	for rel in relations:
+		rel_id = rel['ID']
+		#CONNECTIVE INFORMATION
+		connective = rel['Connective']['RawText']
+		#TYPE AND SENSE
+		type_relation = rel['Type'] 
+		sense_relation = rel['Sense'][0]
+		#RAWTEXT BOTH ARGS
+		arg1_raw = rel['Arg1']['RawText']
+		arg2_raw = rel['Arg2']['RawText']
+		#TOKENLIST FOR BOTH ARGS
+		arg1_token_list = rel['Arg1']['TokenList']
+		arg2_token_list = rel['Arg1']['TokenList']
+		#AMOUNT OF ARGUMENTS FOR THE TOKENS
+		arg1_amount_tokens = max(zip(*rel['Arg1']['TokenList'])[-1])
+		arg2_amount_tokens = max(zip(*rel['Arg2']['TokenList'])[-1])
+		#TAGGED TOKENS
+		tags = []
+		for sent in en_parse_dict[rel['DocID']]['sentences']:
+		    words, rest = zip(*sent['words'])
+		    pos_tags = [el['PartOfSpeech'] for el in rest]
+		    # tags.append(pos_tags)
+		    tags.append(zip(words, pos_tags))
+
+		# zip(*en_parse_dict[rel['DocID']]['sentences'][0]['words'])[0]
+
+	
+	#ALL FEATURES SOME OF WHICH CAN'T BE VECTORIZED
+	all_features.append({'id':rel_id, 'connective': connective, 'type': type_relation, 'arg1_raw':arg1_raw})
 
 
-some_features = []
-for rel in relations:
-	rel_id = rel['ID']
-	#CONNECTIVE INFORMATION
-	connective = rel['Connective']['RawText']
-	#TYPE AND SENSE
-	type_relation = rel['Type'] 
-	sense_relation = rel['Sense'][0]
-	#RAWTEXT BOTH ARGS
-	arg1_raw = rel['Arg1']['RawText']
-	arg2_raw = rel['Arg2']['RawText']
-	#TOKENLIST FOR BOTH ARGS
-	arg1_token_list = rel['Arg1']['TokenList']
-	arg2_token_list = rel['Arg1']['TokenList']
-	#AMOUNT OF ARGUMENTS FOR THE TOKENS
-	arg1_amount_tokens = max(zip(*rel['Arg1']['TokenList'])[-1])
-	arg2_amount_tokens = max(zip(*rel['Arg2']['TokenList'])[-1])
-	#TAGGED TOKENS
-	tags = []
-	for sent in en_parse_dict[rel['DocID']]['sentences']:
-	    words, rest = zip(*sent['words'])
-	    pos_tags = [el['PartOfSpeech'] for el in rest]
-	    # tags.append(pos_tags)
-	    tags.append(zip(words, pos_tags))
-	# zip(*en_parse_dict[rel['DocID']]['sentences'][0]['words'])[0]
-
-
-# 	# print connective, '\t\t',type_relation,'\t\t',sense_relation, '\t\t', arg1_raw
-	# some_features.append({rel['ID']:rel['Connective'], 'tokens_connective':rel['Connective']['TokenList']})
-
-	#SOME INFO THAT CAN BE VECTORIZED
+	# SOME INFO THAT CAN BE VECTORIZED
 	some_features.append({'id':rel_id, 'connective': connective, 'type': type_relation, 'arg1_raw':arg1_raw})
 
 
-print some_features
+extract_data()
 
 
 
@@ -67,8 +69,6 @@ print some_features
 # print "THESE ARE THE WORDS"
 # for el in en_parse_dict[en_doc_id]['sentences'][15]['words']:
 # 	print el
-
-
 
 
 #VECTORIZER ASKS FOR LIST OF DICTIONAIRY CONTAINING KEY AND VALUE. VALUE CAN'T BE LIST
