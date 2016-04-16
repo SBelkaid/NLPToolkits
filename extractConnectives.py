b0e11afbe92150a492dc2c03aa2d064e3ed3cb3e
Soufyan Belkaid <soufyanbelkaid@MacBook-Pro-2.local>
2016-04-16 17:26:09 +0200

    All leon's data extraction in one script. It's faster now, because the parse file is loaded once

--- extractConnectives.py
+++ extractConnectives.py
@@ -2,78 +2,150 @@ import json
 import codecs
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from collections import defaultdict
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.classify import maxent
+from collections import deque
 
-plt.style.use('ggplot')
+# plt.style.use('ggplot')
 ENGLISH_TRIAL = 'tutorial/conll16st-en-01-12-16-trial'
-ENGLISH_TRAIN = 'conll16st-en-zh-dev-train_LDC2016E50/conll16st-en-01-12-16-train'
+ENGLISH_TRAIN = 'conll16st-en-zh-dev-train_LDC2016E50/conll16st-en-01-12-16-train/'
 PATH_RAW_DOCS = 'conll16st-en-zh-dev-train_LDC2016E50/conll16st-en-01-12-16-train/raw/'
 
 #SET THE USE VARIABLE TO EITHER ENGLISH TRIAL OR THE ENGLISH TRAIN
 USE = ENGLISH_TRAIN
 
-#LOADING DATA
-# pdtb_file = codecs.open(USE+'/relations.json', encoding='utf8')
-# relations = [json.loads(x) for x in pdtb_file]
-# en_parse_dict = json.load(codecs.open(USE+'/parses.json', encoding='utf8'))
-# file_names = os.listdir(PATH_RAW_DOCS)
-# tfidf_all_files = [codecs.open(PATH_RAW_DOCS+f, encoding='latin-1').read() for f in file_names]
-# tfidf = TfidfVectorizer(min_df=1, encoding='latin-1', stop_words='english')
-# X = tfidf.fit_transform(tfidf_all_files)
+def readFile(path_to_json):
+	with open(path_to_json) as input_file:
+		all_input = json.load(input_file)
+	return all_input
 
-# for file_id in en_parse_dict.keys():
-	# for file_sentences in en_parse_dict[file_id]['sentences']:
-		# print sent
-		# break
+def get_part_of_speech(docID, find_word, span):
+    """
+    Get PoS from the parses.json file by filename, word and span.
+    """
+    sentencelist = enumerate(PARSES[docID]["sentences"])
+    for sentenceID, sentence in sentencelist:
+        for word_data in sentence["words"]:
+            word = word_data[0]                
+            off_begin = word_data[1]["CharacterOffsetBegin"]
+            off_end = word_data[1]["CharacterOffsetEnd"]
+            part_of_speech = word_data[1]["PartOfSpeech"]
+            begin, end = span
+            if off_begin == begin and off_end == end:
+                return part_of_speech
+
+def get_phrase_structure(docID, sentenceID):
+    """
+    Retrieve phrase_structure from the parses.json file by filename and sentenceID.
+    """
+    sentencelist = PARSES[docID]["sentences"]
+    phrase_structure = sentencelist[sentenceID]["parsetree"]
+    return phrase_structure
+
+def PS_or_SS(arg1_sentenceID, sentenceID):
+    """
+    Returns Arg1 type (PS or SS) based on sentenceIDs.
+    """
+    if arg1_sentenceID == sentenceID:
+        return "SS"
+    elif arg1_sentenceID == sentenceID-1:
+        return "PS"
+
+def constructTrainingData(list_of_relations):
+	#To find all the connectives and store them in a list
+	connectivelist = []
+	tuplelist = []
+	#iterate through the file:
+	for relationID, relation in enumerate(list_of_relations):
+		connective = relation["Connective"]["RawText"]
+		connective_type = relation["Type"]
+		if connective_type != 'Explicit':
+			continue
+		docID = relation["DocID"]
+		tokenlist = relation["Connective"]["TokenList"]
+		sense = relation["Sense"][0]
+		connective_extent = relation["Connective"]["CharacterSpanList"][0]
+		tokenID = tokenlist[0][2]
+		sentenceID = tokenlist[0][3]
+		sentence_position = tokenlist[0][4]
+		arg1 = relation["Arg1"]
+		arg1_extent = arg1["CharacterSpanList"][0]
+		arg1_sentenceID = arg1["TokenList"][0][3]
+		arg1_type = PS_or_SS(arg1_sentenceID, sentenceID) #get arg1_type
+		if arg1_type == "PS":
+			arg1_phrase_structure = get_phrase_structure(docID, arg1_sentenceID)
+		else:
+			arg1_phrase_structure = None
+		arg2 = relation["Arg2"]
+		arg2_extent = arg2["CharacterSpanList"][0]
+		arg2_sentenceID = arg2["TokenList"][0][3]
+		part_of_speech = get_part_of_speech(docID, connective, connective_extent)
+		phrase_structure = get_phrase_structure(docID, sentenceID)
+		# print docID, sentenceID, part_of_speech, connective
+		dependency_heading, dependency_attached = get_connective_dependency(docID, sentenceID, part_of_speech, connective)
+		connectivelist.append(connective.lower())
+		# print(docID, #filename
+		# 	tokenID, #unique token ID
+		# 	sentenceID, #sentenceID
+		# 	sentence_position, #position in sentence
+		# 	connective, #token
+		# 	connective_extent, #extent connective
+		# 	part_of_speech, #PoS
+		# 	dependency_heading, #what is the head of the connective
+		# 	dependency_attached, #what is attached to the connectiv
+		# 	phrase_structure, #phrase structure
+		# 	arg1_extent, #extent arg1
+		# 	arg1_type, #PS or SS
+		# 	arg1_phrase_structure, #arg1 phrase structure
+		# 	arg2_extent, #extent arg2
+		# 	sense, #meaning
+		# 	relationID #discourse relation ID
+		# 	)
+		tuplelist.append(({'connective': connective, 'pos': part_of_speech, 'position': sentence_position}, 1))
+	return tuplelist
+
+def get_connective_dependency(docID, sentenceID, part_of_speech, connective):
+    """
+    Return heading and attached dependencies of connectives. 
+    """
+    dependency_heading = "_"
+    dependency_attached = "_"
+    sentencelist = PARSES[docID]["sentences"]
+    dependencylist = sentencelist[sentenceID]["dependencies"]
+    for dependency in dependencylist:
+        pos, heading, attached = dependency
+        heading_token, headingID = heading.split("-")[:2]
+        attached_token, attachedID = attached.split("-")[:2]
+        if attached_token == connective:
+            dependency_heading = heading
+        if heading_token == connective:
+            dependency_attached = attached
+
+    return dependency_heading, dependency_attached 
 
 
+if __name__ == '__main__':
+	KNOWN_RELATIONS = [json.loads(line) for line in open(ENGLISH_TRAIN+'relations.json', 'r')]
+	PARSES = readFile(ENGLISH_TRAIN+'parses.json')
+	training_data = constructTrainingData(KNOWN_RELATIONS)
 
-# train = [
-#     ({'a': 1, 'b': 1, 'c': 1}, 'y'),
-#     ({'a': 5, 'b': 5, 'c': 5}, 'x'),
-#     ({'a': 0.9, 'b': 0.9, 'c': 0.9}, 'y'),
-#     ({'a': 5.5, 'b': 5.4, 'c': 5.3}, 'x'),
-#     ({'a': 0.8, 'b': 1.2, 'c': 1}, 'y'),
-#     ({'a': 5.1, 'b': 4.9, 'c': 5.2}, 'x')
-# ]
+	test =  [
+	    {'connective': 'until', 'pos': 'IN', 'position': 18},
+	    {'connective': 'until', 'pos': 'IN', 'position': 6},
+	    {'connective': 'before', 'pos': 'IN', 'position': 13},
+	    {'connective': 'Moreover', 'pos': 'RB', 'position': 0},
+	    {'connective': 'but', 'pos': 'CC', 'position': 16},
+	    {'connective': 'after', 'pos': 'IN', 'position': 21},
+	    {'connective': 'Testing', 'pos': 'XXX', 'position': 90}
+	  ]
 
-train = [({'connective': 'unless', 'pos': 'IN', 'position': 12, 'bla': [1,2,3]}, 1),
- ({'connective': 'But', 'pos': 'CC', 'position': 0, 'bla': [1,3]}, 1),
- ({'connective': 'also', 'pos': 'RB', 'position': 3, 'bla': [1,2,3]}, 1),
- ({'connective': 'until', 'pos': 'IN', 'position': 20, 'bla': [1,2,3]}, 1),
- ({'connective': 'as', 'pos': 'IN', 'position': 6, 'bla': [1,2,3]}, 1),
- ({'connective': 'and', 'pos': 'CC', 'position': 14, 'bla': [1,2,3]}, 1),
- ({'connective': 'until', 'pos': 'IN', 'position': 18, 'bla': [1,2,3]}, 1),
- ({'connective': 'until', 'pos': 'IN', 'position': 6, 'bla': [1,2,3]}, 1),
- ({'connective': 'before', 'pos': 'IN', 'position': 13, 'bla': [1,2,3]}, 1),
- ({'connective': 'Moreover', 'pos': 'RB', 'position': 0, 'bla': [1,2,3]}, 1),
- ({'connective': 'Test', 'pos': 'XX', 'position': 90, 'bla': [1,2,3,6,7]}, 0)
-        ]
+	encoding = maxent.TypedMaxentFeatureEncoding.train(
+	    training_data, count_cutoff=3, alwayson_features=True)
 
-# test = [
-#     {'a': 5.2, 'b': 5.1, 'c': 5},
-#     {'a': 1, 'b': 0.8, 'c': 1.2},
-#     {'a': 5.2, 'b': 5.1, 'c': 5}
-#     ]
+	classifier = maxent.MaxentClassifier.train(
+	    training_data, bernoulli=False, encoding=encoding, trace=0)
 
-test =  [
-    {'connective': 'until', 'pos': 'IN', 'position': 18},
-    {'connective': 'until', 'pos': 'IN', 'position': 6},
-    {'connective': 'before', 'pos': 'IN', 'position': 13},
-    {'connective': 'Moreover', 'pos': 'RB', 'position': 0},
-    {'connective': 'but', 'pos': 'CC', 'position': 16},
-    {'connective': 'after', 'pos': 'IN', 'position': 21},
-    {'connective': 'Testing', 'pos': 'XXX', 'position': 90}
-  ]
-
-encoding = maxent.TypedMaxentFeatureEncoding.train(
-    train, count_cutoff=3, alwayson_features=True)
-
-classifier = maxent.MaxentClassifier.train(
-    train, bernoulli=False, encoding=encoding, trace=0)
-
-classifier.classify_many(test)
+	print classifier.classify_many(test)
