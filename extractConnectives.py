1a4d279aba5283490e3efd10df17111720d31d77
Soufyan Belkaid <soufyanbelkaid@MacBook-Pro-2.local>
2016-04-17 17:18:40 +0200

    The script now also extracts candidate cues by using ngrams and comparing them with the know connectives

--- extractConnectives.py
+++ extractConnectives.py
@@ -7,8 +7,8 @@ from collections import defaultdict
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.classify import maxent
-from collections import deque
-
+from nltk.classify.util import accuracy
+from sklearn.feature_extraction.text import CountVectorizer
 # plt.style.use('ggplot')
 ENGLISH_TRIAL = 'tutorial/conll16st-en-01-12-16-trial'
 ENGLISH_TRAIN = 'conll16st-en-zh-dev-train_LDC2016E50/conll16st-en-01-12-16-train/'
@@ -16,13 +16,9 @@ PATH_RAW_DOCS = 'conll16st-en-zh-dev-train_LDC2016E50/conll16st-en-01-12-16-trai
 
 #SET THE USE VARIABLE TO EITHER ENGLISH TRIAL OR THE ENGLISH TRAIN
 USE = ENGLISH_TRAIN
+ANALYZER = CountVectorizer(ngram_range=(1,8), analyzer='word').build_analyzer()
 
-def readFile(path_to_json):
-	with open(path_to_json) as input_file:
-		all_input = json.load(input_file)
-	return all_input
-
-def get_part_of_speech(docID, find_word, span):
+def get_part_of_speech(docID, span):
     """
     Get PoS from the parses.json file by filename, word and span.
     """
@@ -54,6 +50,23 @@ def PS_or_SS(arg1_sentenceID, sentenceID):
     elif arg1_sentenceID == sentenceID-1:
         return "PS"
 
+def discourseConnectives(return_list=True):
+	"""
+	return a dictionairy of all the possible connective senses and how they occur
+	by putting the words indicating the type in a set.
+	"""
+	connectives = defaultdict(set)
+	list_of_connectives = list()
+	for rel in KNOWN_RELATIONS:
+		if rel['Type']=='Explicit':
+				connectives[rel['Sense'][0]].add(rel['Connective']['RawText'].lower())
+	if return_list:
+		for key, val in connectives.items():
+			for v in val:
+				list_of_connectives.append((v, key))
+		return list_of_connectives
+	return connectives
+
 def constructTrainingData(list_of_relations):
 	#To find all the connectives and store them in a list
 	connectivelist = []
@@ -82,11 +95,16 @@ def constructTrainingData(list_of_relations):
 		arg2 = relation["Arg2"]
 		arg2_extent = arg2["CharacterSpanList"][0]
 		arg2_sentenceID = arg2["TokenList"][0][3]
-		part_of_speech = get_part_of_speech(docID, connective, connective_extent)
+		part_of_speech = get_part_of_speech(docID, connective_extent)
 		phrase_structure = get_phrase_structure(docID, sentenceID)
 		# print docID, sentenceID, part_of_speech, connective
 		dependency_heading, dependency_attached = get_connective_dependency(docID, sentenceID, part_of_speech, connective)
 		connectivelist.append(connective.lower())
+		features = {'connective': connective,
+		 'pos': part_of_speech,
+		 'position': sentence_position,
+		 'dependency':dependency_heading,
+		 'phrase_structure':phrase_structure}
 		# print(docID, #filename
 		# 	tokenID, #unique token ID
 		# 	sentenceID, #sentenceID
@@ -104,7 +122,7 @@ def constructTrainingData(list_of_relations):
 		# 	sense, #meaning
 		# 	relationID #discourse relation ID
 		# 	)
-		tuplelist.append(({'connective': connective, 'pos': part_of_speech, 'position': sentence_position}, 1))
+		tuplelist.append((features, 1))
 	return tuplelist
 
 def get_connective_dependency(docID, sentenceID, part_of_speech, connective):
@@ -124,28 +142,61 @@ def get_connective_dependency(docID, sentenceID, part_of_speech, connective):
         if heading_token == connective:
             dependency_attached = attached
 
-    return dependency_heading, dependency_attached 
+    return dependency_heading, dependency_attached
+
+def comparePhrases(sorted_ngrams, mapping_connectives):
+	candidates = []
+	for gram in sorted_ngrams:
+		if gram in zip(*mapping_connectives)[0]:
+			candidates.append(gram)
+	return candidates
+
+def extractCandidates(mapping_connectives):
+	"""
+	Find candidate connectives, if it signals discourse structure then it's a candidate.
+	Extract n-grams to find phrases in the parsed data. 
+	"""
+
+	for docID in PARSES:
+		sentences = PARSES[docID]['sentences']
+		print '\n\n\n\n',docID
+		print "amount of sentences: {}".format(len(sentences))
+		for sent in sentences:
+			# print "amount of words: {}".format(len(sent['words']))
+			# print "raw sentence: {}".format(' '.join([el[0] for el in sent['words']]))
+			tagged = [(el[0],el[1]['PartOfSpeech']) for el in sent['words']]
+			print tagged, '\n\n\n\n'
+			tokens = zip(*sent['words'])[0]
+			sentence = ' '.join(tokens)
+			ngrams = ANALYZER(sentence)
+			sorted_on_length = sorted(ngrams, key=lambda x:len(x))
+			candidate_cues = comparePhrases(sorted_on_length, mapping_connectives)
+
+		break
 
 
 if __name__ == '__main__':
 	KNOWN_RELATIONS = [json.loads(line) for line in open(ENGLISH_TRAIN+'relations.json', 'r')]
-	PARSES = readFile(ENGLISH_TRAIN+'parses.json')
-	training_data = constructTrainingData(KNOWN_RELATIONS)
+	PARSES = json.load(open(ENGLISH_TRAIN+'parses.json', 'r'))
+	unique_conn_mapping = discourseConnectives()
+	extractCandidates(unique_conn_mapping)
+	# training_data = constructTrainingData(KNOWN_RELATIONS)
 
-	test =  [
-	    {'connective': 'until', 'pos': 'IN', 'position': 18},
-	    {'connective': 'until', 'pos': 'IN', 'position': 6},
-	    {'connective': 'before', 'pos': 'IN', 'position': 13},
-	    {'connective': 'Moreover', 'pos': 'RB', 'position': 0},
-	    {'connective': 'but', 'pos': 'CC', 'position': 16},
-	    {'connective': 'after', 'pos': 'IN', 'position': 21},
-	    {'connective': 'Testing', 'pos': 'XXX', 'position': 90}
-	  ]
+	# test =  [
+	#     {'connective': 'until', 'pos': 'IN', 'position': 18},
+	#     {'connective': 'until', 'pos': 'IN', 'position': 6},
+	#     {'connective': 'before', 'pos': 'IN', 'position': 13},
+	#     {'connective': 'Moreover', 'pos': 'RB', 'position': 0},
+	#     {'connective': 'but', 'pos': 'CC', 'position': 16},
+	#     {'connective': 'after', 'pos': 'IN', 'position': 21},
+	#     {'connective': 'Testing', 'pos': 'XXX', 'position': 90}
+	#   ]
 
-	encoding = maxent.TypedMaxentFeatureEncoding.train(
-	    training_data, count_cutoff=3, alwayson_features=True)
+	# encoding = maxent.TypedMaxentFeatureEncoding.train(
+	#     training_data, count_cutoff=3, alwayson_features=True)
 
-	classifier = maxent.MaxentClassifier.train(
-	    training_data, bernoulli=False, encoding=encoding, trace=0)
+	# classifier = maxent.MaxentClassifier.train(
+	#     training_data, bernoulli=False, encoding=encoding, trace=0)
 
-	print classifier.classify_many(test)
+	# # print classifier.classify_many(test)
+
